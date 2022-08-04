053689042fa35232088340880089e4dbeba76b27
Nico Bokhari <nebokha@umich.edu>
2022-08-04 19:43:38 -0400

    Added screen size and color settings

--- settings.py
+++ settings.py
@@ -1,1 +1,12 @@
 """Module will hold settings"""
+#easier to modify game's appearance and behavior as project grows
+
+class Settings:
+    """Store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initalize Game Settings"""
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
