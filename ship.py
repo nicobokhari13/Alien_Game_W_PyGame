ef5ae063240795cfabc2737f47ceb6588f1b80e3
Nico Bokhari <nebokha@umich.edu>
2022-08-04 20:24:40 -0400

    Create ship.py and ship code

---
+++ ship.py
@@ -0,0 +1,24 @@
+"""Contains Ship Module for Alien Invasion Game"""
+
+import pygame
+
+class Ship: 
+    """Manages the Ship"""
+
+    def __init__(self, ai_game): #ship is created after the game instance is created
+        """Initialize the ship and set start coordinates"""
+
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect() #treat all game elements like rectangles
+
+        #Start each new ship at the bottom center of the screen 
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at current location"""
+        self.screen.blit(self.image, self.rect)
+
